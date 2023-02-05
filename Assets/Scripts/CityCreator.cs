269823e01a68523acbe7b1396a3ffc0edae91739
AlexanderCrane <InnovateTheWorld@yahoo.com>
2023-02-04 22:52:21 -0600

    Added random city name generation

--- Assets/Scripts/CityCreator.cs
+++ Assets/Scripts/CityCreator.cs
@@ -7,10 +7,14 @@ public class CityCreator : MonoBehaviour
     public GameObject mushroomCityPrefab;
     public bool placingCity = true; // TODO: This should change to be false, and only set to true when user initiates city placement
 
+    string[] namePrefixes = new string[] { "Spore", "Fungus", "Myco", "Toadstool", "Mold", "Truffle", "Rot", "Muck" };
+    string[] nameSuffixes = new string[] { "burgh", "sbury", "by", " Town", " City", " Village", "ton", "ville" };
+    List<string> usedCityNames;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        usedCityNames = new List<string>();
     }
 
     // Update is called once per frame
@@ -22,7 +26,26 @@ public class CityCreator : MonoBehaviour
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
 
             if (Physics.Raycast(ray, out hit)) {
-                GameObject.Instantiate(mushroomCityPrefab, hit.point, mushroomCityPrefab.transform.rotation);
+                string prefix = namePrefixes[Random.Range(0, namePrefixes.Length)];
+                string suffix = nameSuffixes[Random.Range(0, nameSuffixes.Length)];
+                string cityName = prefix + suffix;
+
+                while(true)
+                {
+                    if(usedCityNames.Contains(cityName))
+                    {
+                        cityName = "New " + cityName;
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+                
+                usedCityNames.Add(cityName);
+
+                GameObject city = GameObject.Instantiate(mushroomCityPrefab, hit.point, mushroomCityPrefab.transform.rotation);
+                city.GetComponent<CityManager>().townName.text = cityName;
                 
                 // Do something with the object that was hit by the raycast.
             }
---
+++ Assets/Scripts/CityManager.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class CityManager : MonoBehaviour
+{
+    public TextMeshProUGUI townName;
+ 
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
